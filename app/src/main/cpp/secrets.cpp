6061a3e2a43e246411463ad4e6fc94695702ebb0
Artem Kurantsov <artsemkurantsou@gmail.com>
2024-04-06 00:50:36 +0100

    native impl update

--- app/src/main/cpp/secrets.cpp
+++ app/src/main/cpp/secrets.cpp
@@ -1,8 +1,7 @@
 #include "secrets.h"
 
-static int api_key_bytes API_KEY_BYTES_DEFINITION;
-
 void getApiKey(char* buffer) {
+    int api_key_bytes API_KEY_BYTES_DEFINITION;
     for (int i = 0; i < API_KEY_LENGTH; i++) {
         buffer[i] = api_key_bytes[i] ^ XOR_VALUE;
     }
